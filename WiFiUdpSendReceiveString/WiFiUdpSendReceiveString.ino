d4eeb9b8e49a6ca06c14d0a25bbe5384cb6c8305
Santi <scros97@gmail.com>
2020-02-03 18:25:37 +0100

    No esperar serial

--- WiFiUdpSendReceiveString/WiFiUdpSendReceiveString.ino
+++ WiFiUdpSendReceiveString/WiFiUdpSendReceiveString.ino
@@ -42,9 +42,6 @@ boolean active = false;
 void setup() {
   //Initialize serial and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
 
   // check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
